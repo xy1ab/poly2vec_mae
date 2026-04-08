7b6ac3d48802259d088333ccf094cadf8f2f061a
hubin <hubin@zhejianglab.org>
2026-04-08 11:27:20 +0800

    delete_ae

--- ae_pretrain/src/losses/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Loss package for AE pretraining."""
-
-from .recon_mag_phase import compute_mag_phase_losses
-
-__all__ = ["compute_mag_phase_losses"]
--- ae_pretrain/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Test package for ae_pretrain.
-
-This file enables package-relative imports inside tests so static analyzers can
-resolve project modules without requiring external IDE configuration.
-"""
